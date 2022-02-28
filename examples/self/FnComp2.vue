751918693bbf288066cadd62efe0a9c8bfe182c2
chest <630601642@qq.com>
2022-02-28 13:43:43 +0800

    feat: 7.bak.

---
+++ examples/self/FnComp2.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <div>
+    FnComp====2 {{props.title}}
+    <br>
+    {{data}}
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FnComp2'
+}
+</script>
+
+<style scoped>
+
+</style>
