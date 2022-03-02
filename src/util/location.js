764a75467c0f965581a8213113b8e15ddd1c536c
homewin <chest2333@gmail.com>
2022-03-02 23:10:56 +0800

    feat: comments

--- src/util/location.js
+++ src/util/location.js
@@ -7,11 +7,20 @@ import { fillParams } from './params'
 import { warn } from './warn'
 import { extend } from './misc'
 
+/**
+ * 此函数是处理例如 $router.push({path: '/detail', query: {id: 1}}) 里的参数的
+ * 返回一个 { _normalized,path,query,hash  }, row主要有4种情况
+ * 1. 包含name，不含path的对象，如{name: 'detail', params: {id: 1}}
+ * 2. 不含path和name，但是含有params的对象，这种路由形式用来进行仅params发生变化的相对路由，如{params: {id: 1}}
+ * 3. 字符串形式，如'/detail'，就是直接用路径进行路由的场景
+ * 4. 不含name，含path的对象，如{path: '/detail', query: {id: 1}}
+ * */
+
 export function normalizeLocation (
-  raw: RawLocation,
-  current: ?Route,
-  append: ?boolean,
-  router: ?VueRouter
+  raw: RawLocation, // 要进行正规化处理的原始数据
+  current: ?Route,  // 当前的路由对象
+  append: ?boolean, // 是否为追加模式，与path解析相关，主要是给router-link提供的
+  router: ?VueRouter // vue-router的实例对象
 ): Location {
   let next: Location = typeof raw === 'string' ? { path: raw } : raw
   // named target
